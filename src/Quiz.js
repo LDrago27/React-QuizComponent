c11b203f939e398b20ffca74ee043ed64f1254de
LDrago27 <38322563+LDrago27@users.noreply.github.com>
2022-03-25 19:54:18 +0530

    fix

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component{
         this.setState({quiz_position:this.state.quiz_position+1});
     }
     handleReset(){
-        this.state = {quiz_position:1};
+        this.setState({quiz_position:1});
     }
     render(){
         const isQuizEnd = this.state.quiz_position-1 === quizData.quiz_questions.length
